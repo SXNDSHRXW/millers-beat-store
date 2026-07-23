1c7ccd6c8045ff365014af9ebd0eb6c9832157ba
SXNDSHRXW <millersbeats@gmail.com>
2026-07-23 18:09:50 +0100

    Use Stripe fetch HTTP client for Cloudflare Workers compatibility

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -6,6 +6,7 @@ function getStripe() {
   if (!_stripe) {
     _stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
       apiVersion: '2025-02-24.acacia',
+      httpClient: Stripe.createFetchHttpClient(),
     });
   }
   return _stripe;
